a7f706052bf46a1e9022c99fe259df14a1490d94
Om chuphal <hydragaming042@gmail.com>
2025-11-30 22:00:31 +0530

    Define Record struct and function prototypes in violation.h

--- violation.h
+++ violation.h
@@ -1,1 +1,19 @@
+#ifndef VIOLATION_H
+#define VIOLATION_H
 
+typedef struct {
+    char plate[20];
+    char type[60];
+    float fine;
+    int paid;
+    int repeat_count;
+} Record;
+
+void loadData(Record *list, int *size);
+void saveData(Record *list, int size);
+void addRecord(Record *list, int *size);
+void listRecords(Record *list, int size);
+void payFine(Record *list, int size);
+void summaryReport(Record *list, int size);
+
+#endif
